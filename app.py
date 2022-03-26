004fb7924df26bf230e04ecf6f019b6afbb76316
nemkoruss <isaev.m1988@gmail.com>
2022-03-27 01:11:57 +0300

    Изменение в расчетах пленки

--- app.py
+++ app.py
@@ -53,11 +53,19 @@ if b == 'Замес гранулы':
     st.write('Стрейч = ' + str(vstz) + ' кг.')
     st.write('Мел = ' + str(hz) + ' кг.')
     st.write('Краситель = ' + str(iz) + ' кг.')
-    if st.button('Say hello'):
-        st.write('Why hello there')
-    else:
-        st.write('Goodbye')
-
+    @st.cache
+     def convert_df(df):
+         # IMPORTANT: Cache the conversion to prevent computation on every rerun
+         return df.to_csv().encode('utf-8')
+    
+    csv = convert_df(my_large_df)
+    
+    st.download_button(
+         label="Download data as CSV",
+         data=csv,
+         file_name='large_df.csv',
+         mime='text/csv',
+     )
     st.title('Себес. замеса грaнулы: ')
     col3, col4 = st.beta_columns(2)
     with col3:
