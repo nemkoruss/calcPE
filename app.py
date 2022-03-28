101c7a96648226cdee990f428ca141aa8603d114
nemkoruss <isaev.m1988@gmail.com>
2022-03-28 18:55:01 +0300

    Мелкие правки

--- app.py
+++ app.py
@@ -474,11 +474,11 @@ if y == "Заказ на пакеты":
     if aza11 == 2:
             aza11 = 2
     elif aza11 == 1:
-            aza11 = 1
-    elif aza11 == 0:
+#           aza11 = 1
+#    elif aza11 == 0:
         aza11 = 1
-    else:
-        aza11 < 0
+#    else:
+#        aza11 < 0
     with col25:
         bza11 = st.number_input('[Толстая плёнка = 1, Тонкая плёнка = 2, Флекс-печать = 3]')
     if bza11 == 1:
@@ -487,10 +487,10 @@ if y == "Заказ на пакеты":
             bza11 = 4000
     elif bza11 == 3:
             bza11 = 3000
-    elif bza11 == 0:
-        bza11 = 1
-    else:
-        bza11 < 0
+#    elif bza11 == 0:
+#        bza11 = 1
+#    else:
+#        bza11 < 0
     lza11 = aza11 * bza11
     with col26:
         cza11 = st.number_input('График: [где 12 часов = 1, а 24 часа = 2]: ')
@@ -498,16 +498,16 @@ if y == "Заказ на пакеты":
             cza11 = 1
     elif cza11 == 2:
             cza11 = 2
-    elif cza11 == 0:
-        cza11 = 1
-    else:
-        cza11 < 0
+#    elif cza11 == 0:
+#        cza11 = 1
+#    else:
+#        cza11 < 0
     kza11 = lza11 * cza11
     with col26:
         dza11 = st.number_input('Введите кол-во пакетов в штуках: ')
-    if dza11 == 0:
-        dza11 = 1
-    else:
+#    if dza11 == 0:
+#        dza11 = 1
+#    else:
         dza11 < 0
     sza11 = dza11 / kza11
     if sza11 == 0
