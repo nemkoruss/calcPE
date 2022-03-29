78f92f22eb29658d2cac64cc0c221b003f804fe2
nemkoruss <isaev.m1988@gmail.com>
2022-03-29 19:31:57 +0300

    Мелкие правки

--- app.py
+++ app.py
@@ -67,11 +67,11 @@ if b == 'Замес гранулы':
     with col3:
         cz1 = st.number_input('Цена Мела: ')
         dz1 = st.number_input('Ценa Красителя: ')        
-    az2 = az1 # * az
-    bz2 = bz1 #* bz
-    vst2 = vst1 #* vst
-    cz2 = cz1 #* cz
-    dz2 = dz1 #* dz
+    az2 = az1 * az
+    bz2 = bz1 * bz
+    vst2 = vst1 * vst
+    cz2 = cz1 * cz
+    dz2 = dz1 * dz
     xz = az2 + bz2 + vst2 + cz2 + dz2
     with col3:    
         ez1 = st.number_input('Зарплатa сотрудников: ')
