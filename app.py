1f502c79eeff700f6e375b70c94b3e094c6b110f
nemkoruss <isaev.m1988@gmail.com>
2022-03-29 14:55:05 +0300

    Мелкие правки

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ if a == 'Включено':
     st.markdown('**Интернет магазин: ** [pmpsale.ru](https://pmpsale.ru)')  
     st.markdown('**Скачать приложение: ** [Скачать](https://goo.su/RH7qQ)')
     image = Image.open('./qrcode/qrcode.jpg')
-    st.image(image, width = 70, caption = 'QR код для скачивания',use_column_width=70)
+    st.image(image, width = 200, caption = 'QR код для скачивания',use_column_width=70)
     
 
     
