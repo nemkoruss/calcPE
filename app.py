38f4396a8952bca05d853c35bfdd44a29e25d294
nemkoruss <isaev.m1988@gmail.com>
2022-03-29 14:06:09 +0300

    Мелкие правки

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from PIL import Image
+import cv2
+
 #-----------------------------------------------------------------
 
 a = st.sidebar.radio('ВКЛ/ВЫКЛ "ВВЕДЕНИЕ":', ['Включено','Выключено'])
@@ -19,10 +20,18 @@ if a == 'Включено':
     st.markdown('**Корпоративный сайт: ** [tpkpromed.ru](https://tpkpromed.ru)')
     st.markdown('**Производство бахил: ** [bioinvn.ru](https://bioinvn.ru)')
     st.markdown('**Интернет магазин: ** [pmpsale.ru](https://pmpsale.ru)')  
-    st.markdown('**Скачать приложение: ** [Скачать](https://goo.su/RH7qQ)')
+    st.markdown('**Скачать приложение: ** [Скачать](https://goo.su/RH7qQ)
     
-    img = Image.open(r'qrcode.jpg')
-    img.show()
+    uploaded_file = st.file_uploader("Choose a image file", type="jpg")
+    
+    if uploaded_file is not None:
+        # Convert the file to an opencv image.
+        file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+        opencv_image = cv2.imdecode(file_bytes, 1)
+    
+        # Now do something with the image! For example, let's display it:
+        st.image(opencv_image, channels="BGR")
+    
 
     
 
