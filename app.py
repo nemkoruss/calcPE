2ae2208c9596e0ba3e3e60f47759cae9d0dd22a5
nemkoruss <isaev.m1988@gmail.com>
2022-04-03 23:31:55 +0300

    Мелкие правки

--- app.py
+++ app.py
@@ -10,22 +10,6 @@ names = ['Isaev Maxim']
 usernames = settings.MYSQL_USER,
 passwords = password=settings.MYSQL_PASSWORD,
 
-hashed_passwords = stauth.Hasher(passwords).generate()
-
-authenticator = stauth.Authenticate(names, usernames, hashed_passwords,
-    'some_cookie_name', 'some_signature_key', cookie_expiry_days=30)
-
-
-name, authentication_status, username = authenticator.login('Login', 'main')
-
-
-
-if authentication_status:
-    authenticator.logout('Logout', 'main')
-    st.write('Welcome *%s*' % (name))
-    st.title('Some content')
-
-
 
 
 
@@ -966,8 +950,3 @@ if z == "Цех пакеты":
     st.write('Склад: '  + str(xc111) + ' кор., ' + 'Долг: ' + str(tc111) + ' руб.')
 
 #-------------------------
-
-elif authentication_status == False:
-    st.error('Username/password is incorrect')
-elif authentication_status == None:
-    st.warning('Please enter your username and password')
