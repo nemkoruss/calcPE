4b8920d167c3725daab9aa151d8f7b84ec30e1a5
nemkoruss <isaev.m1988@gmail.com>
2022-04-04 10:20:37 +0300

    Мелкие правки

--- app.py
+++ app.py
@@ -188,10 +188,10 @@ elif x == "Бахилы":
     col9, col10 = st.beta_columns(2)
     with col9:
         ab1 = st.number_input('Вeс ввода ПНД: ')
-        if ab1 == 0:
-            ab1 = 1
-        else:
-            ab1 < 0
+        #if ab1 == 0:
+        #    ab1 = 1
+        #else:
+        #    ab1 < 0
         ab2 = st.number_input('Цeна ПНД: ')
         bb1 = st.number_input('Вeс ввода ПНД вторичка: ')
         bb2 = st.number_input('Цeна ПНД вторичка: ')   
