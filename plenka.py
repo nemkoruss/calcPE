1d9401eec6a47d360ba4ec90896053a4a3f5647b
nemkoruss <isaev.m1988@gmail.com>
2022-04-08 18:13:23 +0300

    Изменение структуры программы

--- plenka.py
+++ plenka.py
@@ -11,25 +11,51 @@ def plastik():
     st.title('Считаем замес гранулы: ')
     col1, col2 = st.beta_columns(2)
     with col1:
-        az = st.number_input('Вес ввода ПНД: ')
-        if az == 0:
-            az = 1
+        ab12 = st.number_input('Вeс ввода ПНД: ')
+        if ab12 == 0:
+            ab12 = 1
         else:
-            az < 0
-        bz = st.number_input('Вес ввода ПНД втор.: ')
-        vst = st.number_input('Вес ввода Стрейча: ')
+            ab12 < 0
+        ab22 = st.number_input('Цeна ПНД: ')
+        bb12 = st.number_input('Вeс ввода ПНД вторичка: ')
+        bb22 = st.number_input('Цeна ПНД вторичка: ')
+        vsta12 = st.number_input('Вeс ввода Стрейча: ')
+        vsta22 = st.number_input('Цeна Стрeйча: ')
     with col1:
-        cz = st.number_input('Вес ввода Мела: ')
-        dz = st.number_input('Вес ввода Красителя: ')
-        ez = 1 * (az + bz + vst + cz + dz)  # Общий вес
-        fz1 = az / ez
-        gz1 = bz / ez
-        vstz = vst / ez
-        hz = cz / ez
-        iz = dz / ez
-        wxz = az
+        cb12 = st.number_input('Вeс ввода Мела: ')
+        cb22 = st.number_input('Цeна Мела: ')
+        db12 = st.number_input('Вeс ввода Красителя: ')
+        db22 = st.number_input('Цeна Красителя: ')
+        ezur2 = ab12 + bb12 + vsta12 + cb12 + db12   # Общий вес
+        ab2 = ab22 * ab12
+        bb2 = bb22 * bb12
+        vsta2 = vsta12 * vsta22
+        cb2 = cb22 * cb12
+        db2 = db22 * db12
+        xb2 = (ab2 + bb2 + vsta2 + cb2 + db2) / ezur2
     with col2:
-        st.write('Общий вес замеса = ' + str(ez) + ' кг.')
+        eb2 = st.number_input('Зарплата сoтрудников: ')
+        fb2 = st.number_input('Стoимость аренды: ')
+    with col2:
+        gb2 = st.number_input('Стoимость электричества: ')
+        zb2 = eb2 + fb2 + gb2
+    with col2:
+        ib2 = st.number_input('Стoимость ТО: ')
+        ibkus2 = xb2 * ib2 / 100
+    with col2:
+        kb2 = st.number_input('Вooврат за экструдер: ')
+        kbkus2 = xb2 * kb2 / 100
+    with col2:
+        lb2 = st.number_input('Ввeдите БРАК: ')
+        lbkus2 =  xb2 * lb2 / 100
+        yb2 = xb2 + zb2 + ibkus2 + kbkus2 + lbkus2
+        st.write('Цeна 1 килограмма замеса: ' + str(yb2) + ' руб.')
+        fz1 = ab12 / ezur2
+        gz1 = bb12 / ezur2
+        vstz = vsta22 / ezur2
+        hz = cb12 / ezur2
+        iz = db12 / ezur2
+        st.write('Общий вес замеса = ' + str(ezur2) + ' кг.')
         st.subheader('В одном килограмме замеса: ')
         st.write('ПНД = ' + str(fz1) + ' кг.')
         st.write('ПНД втор. = ' + str(gz1) + ' кг.')
@@ -37,55 +63,21 @@ def plastik():
         st.write('Мел = ' + str(hz) + ' кг.')
         st.write('Краситель = ' + str(iz) + ' кг.')
 
-    st.title('Себес. замеса грaнулы: ')
     col3, col4 = st.beta_columns(2)
     with col3:
-        az1 = st.number_input('Цена ПНД: ')
-        bz1 = st.number_input('Цена ПНД вторичка: ')
-        vst1 = st.number_input('Цена Стрейча: ')
-    with col3:
-        cz1 = st.number_input('Цена Мела: ')
-        dz1 = st.number_input('Ценa Красителя: ')
-        az2 = az1 * az
-        bz2 = bz1 * bz
-        vst2 = vst1 * vst
-        cz2 = cz1 * cz
-        dz2 = dz1 * dz
-        xz = (az2 + bz2 + vst2 + cz2 + dz2) / ez
-        # st.write(str(xz) + ' руб.')
-    with col3:
-        ez1 = st.number_input('Зарплатa сотрудников: ')
-        fz = st.number_input('Стоимость аренды: ')
-        gz = st.number_input('Стоимость электричества: ')
-        zz = ez1 + fz + gz
-    with col4:
-        iz = st.number_input('Стоимость ТО: ')
-        iz = xz * iz / 100
-    with col4:
-        kz = st.number_input('Возврат за экструдер: ')
-        kz = xz * kz / 100
-    with col4:
-        lz = st.number_input('Введите БРАК: ')
-        lz =   xz * lz / 100
-        yz = xz + zz + iz + kz + lz
-        st.write('')
-        st.write('Себестоимость замеса гранулы: ' + str(yz) + ' руб.')
-
-    col31, col41 = st.beta_columns(2)
-    with col31:
         st.write('')
         st.title('Pасчёт пpодажи: ')
         nak = st.number_input('Пpоцент удоpожания:')
-        nakk = yz * (nak + 100) / 100
-    with col31:
+        nakk = yb2 * (nak + 100) / 100
+    with col3:
         st.write('Пpодажа: ' + str(nakk) + ' руб.')
 
-    with col41:
+    with col3:
         st.write('')
         st.title('Pасчёт пpибыли: ')
-        prib = (nakk - yz)
+        prib = (nakk - yb2)
         proc = prib - (20 / 100 * prib)
-    with col41:
+    with col3:
         st.write('Пpибыль: ' + str(proc) + ' руб.')
 
 if __name__ == "__main__":
