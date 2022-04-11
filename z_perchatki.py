f46ce09b932adbc3e0384a34dc6ff30a5cf7e8f4
nemkoruss <isaev.m1988@gmail.com>
2022-04-11 20:04:01 +0300

    исправление ошибок

--- z_perchatki.py
+++ z_perchatki.py
@@ -1,3 +1,4 @@
+from re import S
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -51,10 +52,8 @@ def perchatki():
         lbkus12 =  xb12 * lb12 / 100
         yb12 = xb12 + zb12 + ibkus12 + kbkus12 + lbkus12
         yb12 = float('{:.3f}'.format(yb12))
-
-    with col15:
-        st.write('')
-        st.write('Цена 1 килограмма плёнки для перчаток: ' + str(yb12) + ' руб.')
+    st.write('')
+    st.write('Цена 1 килограмма плёнки для перчаток: ' + str(yb12) + ' руб.')
 
     st.header('Вeс одной пары перчаток: ')
     col17, col18 = st.columns(2)
@@ -72,16 +71,16 @@ def perchatki():
         fb312 = int (1000)
         zb312 = (ab312 * bb312 * gb312 * db312 * ib312 * fb312) * 2
         zb312 = float('{:.3f}'.format(zb312))
-
-    with col17:
-        st.write('')
-        st.write('Вес одной пары перчаток: ' + str(zb312) + ' гр.')
+    st.write('')
+    st.write('Вес одной пары перчаток: ' + str(zb312) + ' гр.')
 
     st.header('Себестоимость перчаток: ')
     col19, col20 = st.columns(2)
     with col19:
-        mb412 = zb312 * yb12
-        pb412 = mb412 * 1 / 1000
+        mb412 = ((zb312 * yb12) / 100 ) * 1
+        pb412 = ((zb312 * yb12) + mb412) / 1000
+        otho = st.number_input('Отход в %: ')
+        oth = (pb412 /100) * otho
         ab412 = st.number_input('Зaрплата сотрудников: ')
         bb412 = st.number_input('Стоимость Аренды: ')
         cb412 = st.number_input('Стоимость Электричества: ')
@@ -94,24 +93,39 @@ def perchatki():
         else:
             rit12 < 0
         ret12 = jb412 / rit12
-
-        eb412 = 0.005 # Стоимость TO:
-        fb412 = float(0.004) # Стоимость Скотча
-        ib412 = float(0.005) # Стоимость кредита
-        gb412 = float(0.008) # Стоимость пакетов
-        ob4122012 = pb412 + ab412 + bb412 + cb412 + hb412 + ret12
-        const12 = eb412 + fb412 + ib412 + gb412
-        if ob4122012 == 0:
-            consta12 = 0
-        else:
-            consta12 = 1
-        const112 = consta12 * const12
-        ob412812 = ob4122012 + const112
-        ob412812 = float('{:.3f}'.format(ob412812))
-
     with col19:
-        st.write('')
-        st.write('Себестоимость перчаток: ' + str(ob412812) + ' руб.')
+        s_to = st.text_input('Стоимость TO: ', '0.00')
+        eb412 = s_to
+        if eb412 == (''):
+            eb412 = 0
+        else:
+            eb412 = s_to
+    with col19:
+        s_sk = st.text_input('Стоимость Скотча: ', '0.00')
+        fb412 = s_sk
+        if fb412 == (''):
+            fb412 = 0
+        else:
+            fb412 = s_sk
+    with col20:
+        s_kr = st.text_input('Стоимость кредита: ', '0.00')
+        ib412 = s_kr
+        if ib412 == (''):
+            ib412 = 0
+        else:
+            ib412 = s_kr
+    with col20:
+        s_pa = st.text_input('Стоимость пакетов: ', '0.00')
+        gb412 = s_pa
+        if gb412 == (''):
+            gb412 = 0
+        else:
+            gb412 = s_pa
+        ob4122012 = (pb412 - oth) + ab412 + bb412 + cb412 + hb412 + ret12
+        ob412812 = ob4122012 + float(eb412) + float(fb412) + float(ib412) + float(gb412)
+        ob412812 = float('{:.3f}'.format(ob412812))
+    st.write('')
+    st.write('Себестоимость перчаток: ' + str(ob412812) + ' руб.')
 
     col19, col20 = st.columns(2)
     with col19:
@@ -120,10 +134,8 @@ def perchatki():
         nak212 = st.number_input('Процент удорожания:')
         nakk212 = ob412812 * (nak212 + 100) / 100
         nakk212 = float('{:.3f}'.format(nakk212))
-
-    with col19:
-        st.write('')
-        st.write('Продажа: ' + str(nakk212) + ' руб.')
+    st.write('')
+    st.write('Продажа: ' + str(nakk212) + ' руб.')
 
     with col19:
         st.write('')
@@ -141,28 +153,29 @@ def perchatki():
         ras123 = float('{:.3f}'.format(ras123))
         are123 = float('{:.3f}'.format(are123))
         kre123 = float('{:.3f}'.format(kre123))
-
-    with col19:
-        st.write('')
-        st.write('Пpибыль: ' + str(proc123) + ' руб.')
-        st.write('Офис: ' + str(ofi123) + ' руб.')
-        st.write('Налог: ' + str(nal123) + ' руб.')
-        st.write('Аренда: ' + str(ras123) + ' руб.')
-        st.write('Расходы: ' + str(are123) + ' руб.')
-        st.write('Кредит: ' + str(kre123) + ' руб.')
+    st.write('')
+    st.write('Пpибыль: ' + str(proc123) + ' руб.')
+    st.write('Офис: ' + str(ofi123) + ' руб.')
+    st.write('Налог: ' + str(nal123) + ' руб.')
+    st.write('Аренда: ' + str(ras123) + ' руб.')
+    st.write('Расходы: ' + str(are123) + ' руб.')
+    st.write('Кредит: ' + str(kre123) + ' руб.')
 
     with open('./txt/info.txt', 'a+', encoding = 'utf8') as file:
         if st.sidebar.button('Записать результат'):
-            file.write('Цена 1 килограмма плёнки для перчаток: ' + str(yb12) + ' руб.' '\n' '\n'
+            file.write(
+                'Цена 1 килограмма плёнки для перчаток: ' + str(yb12) + ' руб.' '\n' '\n'
                 'Вес одной пары перчаток: ' + str(zb312) + ' гр.' '\n' '\n'
                 'Себестоимость перчаток: ' + str(ob412812) + ' руб.' '\n' '\n'
+                #'Константы ' + str(X) + '' '\n' '\n'
                 'Продажа: ' + str(nakk212) + ' руб.' '\n'
                 'Пpибыль: ' + str(proc123) + ' руб.' '\n'
                 'Офис: ' + str(ofi123) + ' руб.' '\n'
                 'Расходы: ' + str(nal123) + ' руб.' '\n'
                 'Аренда: ' + str(ras123) + ' руб.' '\n'
                 'Пpибыль: ' + str(are123) + ' руб.' '\n'
-                'Кредит: ' + str(kre123) + ' руб.' '\n' '\n')
+                'Кредит: ' + str(kre123) + ' руб.' '\n' '\n'
+                )
 
     with open('./txt/info.txt', 'r', encoding = 'utf8') as my_file:
         st.sidebar.download_button(label = 'Скачать результат',
